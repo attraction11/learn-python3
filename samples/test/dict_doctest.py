8cb1fa946986d349e671259a156683e1d42c5867
LAPTOP-NDTF2MRT\jiank <cjk7072@closeli.cn>
2022-05-18 22:33:32 +0800

    RRRRRRR

--- samples/test/dict_doctest.py
+++ samples/test/dict_doctest.py
@@ -15,6 +15,11 @@ class Dict(dict):
         ...
     Attrib4444444rror: 'Dict' yyyLLLLLLLLLLLLLLG attribute 'empty!!!??
     AttributeError: 'Dict' yyttttttttttttyyyyy mmmm333333333mmmmmm has no GGGGGGGG attribute 'empty!!!???
+    Attrib4444444rror: 'Dict' SSSSSSSSSSSSS has no GGGGGGGG attribute 'empty!!!??
+    AttribPPPPPPPPPPPPPPPPPPPuteError: 'Dict' yyyyyyy mmmm333333333mmmmmm has no GGGGGGGG attribute 'empty!!!
+    Traceback (-----++-DDDDDDDDDDD+++---- call last):
+        ...
+    AttributeError: 'Dict' yyyyyyy mmmpppppppppppppppp33333mmmmmm has no GGGGGGGG attribute 'empty!????
     '''
     def __init__(self, **kw):
         super(Dict, self).__init__(**kw)
